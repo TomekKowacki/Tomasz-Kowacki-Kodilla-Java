07ae49068004f014c3604fa6b3a86d1921bfe02b
Tomasz Kowacki <tomek.kowacki@gmail.com>
2022-09-07 19:55:32 +0200

    6.2. Czym jest Test Driven Development (TDD)?

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+//Zadanie: pierwszy test jednostkowy
+        Calculator calculator = new Calculator();
+        int addResult = calculator.addAToB(14, 3);
+        int subtractResult = calculator.subtractAFromB(12, 4);
+    //Adding test:
+        if(addResult == 14 + 3) {
+            System.out.println("Test OK");
+        }
+        else {
+            System.out.println("ERROR");
+        }
+    //Subtract test:
+        if(subtractResult == 4 - 12) {
+            System.out.println("Test OK");
+        }
+        else {
+            System.out.println("ERROR");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    private int a;
+    private int b;
+
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractAFromB(int a, int b) {
+        return b - a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+}
