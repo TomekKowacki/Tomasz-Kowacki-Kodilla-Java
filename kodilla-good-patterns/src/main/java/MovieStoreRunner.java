3682961c959cc9de051727f7ff26095a86a945de
Tomasz Kowacki <tomek.kowacki@gmail.com>
2022-10-28 18:47:26 +0200

    13.1 Funkcyjna iteracja po tytuach

---
+++ kodilla-good-patterns/src/main/java/MovieStoreRunner.java
@@ -0,0 +1,17 @@
+import com.kodilla.good.patterns.challenges.MovieStore;
+
+import java.util.stream.Collectors;
+
+public class MovieStoreRunner {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+
+        String titleString = movieStore.getMovies().entrySet().stream()
+                .flatMap(tittle -> tittle.getValue().stream())
+                .collect(Collectors.joining("!"));
+
+        System.out.println(titleString);
+    }
+}
