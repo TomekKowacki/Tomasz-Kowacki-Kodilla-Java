0af3359db30fde5a9fc4ef0ac476cae6587ca0bf
Tomasz Kowacki <tomek.kowacki@gmail.com>
2022-11-07 18:57:18 +0100

    14.3. Konfiguracja przy pomocy klasy Java

--- kodilla-spring/src/main/java/com/example/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/example/kodilla/spring/library/Library.java
@@ -10,9 +10,14 @@ import java.util.List;
 public class Library {
 
     private final List<String> books = new ArrayList<>();
-
-    @Autowired
     private LibraryDbController libraryDbController;
+
+    public Library(final LibraryDbController libraryDbController) {
+        this.libraryDbController = libraryDbController;
+    }
+
+    public Library() {
+    }
 
     public void saveToDb() {
         libraryDbController.saveData();
--- kodilla-spring/src/test/java/com/example/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/example/kodilla/spring/library/LibraryTestSuite.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 @SpringBootTest
 public class LibraryTestSuite {
 
@@ -34,5 +36,18 @@ public class LibraryTestSuite {
 
         //Then
         //do nothing
+    }
+
+    @Test
+    void testContext() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.example.kodilla.spring");
+
+        //When & Then
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ===== Beans list ====");
     }
 }
