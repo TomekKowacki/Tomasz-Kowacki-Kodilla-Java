7c7a48d9fc9efa1daffde09c4128c85a44c4442a
Tomasz Kowacki <tomek.kowacki@gmail.com>
2023-01-19 20:15:25 +0100

    create new module

---
+++ kodilla-patterns2/src/test/java/com/example/kodillapatterns2/KodillaPatterns2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodillapatterns2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaPatterns2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
