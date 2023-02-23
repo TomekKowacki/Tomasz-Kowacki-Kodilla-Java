e1c87049825d43a03d5dbcd65a964ca08ab369df
Tomasz Kowacki <tomek.kowacki@gmail.com>
2023-02-23 18:26:57 +0100

    24.5 Wzorzec Adapter

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/company/SalaryAdapterTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.patterns2.adapter.company;
+
+import com.kodilla.patterns2.adapter.company.oldhrsystem.Workers;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SalaryAdapterTestSuite {
+
+    @Test
+    public void testTotalSalary() {
+        // Given
+        Workers workers = new Workers();
+        SalaryAdapter salaryAdapter = new SalaryAdapter();
+
+        // When
+        double totalSalary = salaryAdapter.TotalSalary(workers.getWorkers(), workers.getSalaries());
+
+        // Then
+        System.out.println(totalSalary);
+        assertEquals(27750, totalSalary, 0);
+    }
+}
