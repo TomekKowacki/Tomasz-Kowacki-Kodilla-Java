8aa60b81c3d8992579d1098c8c8a89bc7bd26af6
Tomasz Kowacki <tomek.kowacki@gmail.com>
2023-03-04 13:14:57 +0100

    30.3. Procedury i funkcje – informacje uzupeniajce

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,39 @@
+package com.kodilla.jdbc;
+
+import org.testng.annotations.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StoredProcTestSuite {
+
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        // Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+
+        // When
+        Statement statement2 = dbManager.getConnection().createStatement();
+        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        statement2.execute(sqlProcedureCall);
+
+        // Then
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(0, howMany);
+        rs.close();
+        statement.close();
+        statement2.close();
+    }
+}
+
