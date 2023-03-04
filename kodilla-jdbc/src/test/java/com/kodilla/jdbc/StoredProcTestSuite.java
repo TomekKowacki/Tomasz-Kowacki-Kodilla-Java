42e8629c21778f071bfe94ae11e5b7156143df7c
Tomasz Kowacki <tomek.kowacki@gmail.com>
2023-03-04 14:01:48 +0100

    30.3 Wywoanie procedury UpdateBestsellers z poziomu Javy

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class StoredProcTestSuite {
 
@@ -31,6 +32,32 @@ public class StoredProcTestSuite {
             howMany = rs.getInt("HOW_MANY");
         }
         assertEquals(0, howMany);
+        rs.close();
+        statement.close();
+        statement2.close();
+    }
+
+    @Test
+    void testUpdateBestsellers() throws SQLException{
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER = false";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=false";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+
+        //When
+        Statement statement2 = dbManager.getConnection().createStatement();
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement2.execute(sqlProcedureCall);
+
+        //Then
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(3, howMany);
         rs.close();
         statement.close();
         statement2.close();
